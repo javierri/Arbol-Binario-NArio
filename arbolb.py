4d1b54500b2615eb125270c8c774788cdd979789
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-10-09 10:49:06 -0400

    Update arbolb.py

--- arbolb.py
+++ arbolb.py
@@ -58,6 +58,62 @@ class Arbolb:
 			return self.elem_mayor(raiz.hder)
 			
 		return raiz.info
+	
+	# TRES FORMAS DISTINTAS DE OBTENER EL NUMERO DE NODOS DE UN ARBOL BINARIO
+	
+	# FORMA 1: la funcion recursiva num_nodos() retorna el numeros de nodos del arbol izquierdo 
+	# y del arbol derecho y los suma al nodo donde se encuentra  
+	
+	def num_nodos(self, raiz = None):
+		if (raiz == None):
+			if (self.__raiz == None):
+				return 0
+			raiz = self.__raiz
+		
+		num = 0	
+		if (raiz.hizq != None):
+			num = self.num_nodos(raiz.hizq)
+					
+		if (raiz.hder != None):
+			num = num + self.num_nodos(raiz.hder)
+		
+		return 1 + num
+
+	# FORMA 2: la funcion recursiva nro_nodos() envia por paranetro el numero de nodos encontrados el recorrido  
+	# (iniciado por cero en la raiz) luego lo envia luego por parametro a sus ramas izquierda y derecha 
+	# para que retroenen la sumen de sus hijos, finalmente retorna el parametro nro mas el nodo actual
+	
+	def nro_nodos (self, raiz = None, nro = 0):
+		if (raiz == None):
+			if (self.__raiz == None):
+				return 0
+			raiz = self.__raiz
+		
+		if (raiz.hizq != None):
+			nro = self.nro_nodos(raiz.hizq,nro)
+					
+		if (raiz.hder != None):
+			nro = self.nro_nodos(raiz.hder,nro)
+		
+		return 1 + nro
+	
+	# FORMA 3 1: la funcion recursiva n_nodos() crea un atributo al arbol self.__n donde sumará los valores
+	# de los nodos a traves del recorrido, al final retorna el valor del atributo	
+	def n_nodos (self, raiz = None):
+		if (raiz == None):
+			self.__n = 0
+			if (self.__raiz == None):
+				return self.__n
+			raiz = self.__raiz
+			
+		self.__n = self.__n + 1
+		if (raiz.hizq != None):
+			self.n_nodos(raiz.hizq)
+					
+		if (raiz.hder != None):
+			self.n_nodos(raiz.hder)
+		
+		return self.__n
 					
 	def preorden (self, raiz = None):
 		if (raiz == None):
